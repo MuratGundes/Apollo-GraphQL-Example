095ad5d84a97b9e6b3f7ee0427b773769290ec14
MuratGundes <murat.gundes@hotmail.com>
2022-11-27 04:39:58 +0100

    add missing dependencies.

--- src/machines/machine.module.ts
+++ src/machines/machine.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MachineResolver } from './machine.resolver';
 import { MachineService } from './machine.service';
+import { DbModule } from '../db/db.module';
 
 @Module({
+  imports: [DbModule],
   providers: [MachineResolver, MachineService],
 })
 export class MachineModule {}
--- src/sensor-data-points/sensor-data-point.module.ts
+++ src/sensor-data-points/sensor-data-point.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SensorDataPointResolver } from './sensor-data-point.resolver';
 import { SensorDataPointService } from './sensor-data-point.service';
+import { DbModule } from '../db/db.module';
 
 @Module({
+  imports: [DbModule],
   providers: [SensorDataPointResolver, SensorDataPointService],
 })
 export class SensorDataPointModule {}
